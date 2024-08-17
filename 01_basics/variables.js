e578c3bf5937d7b506cf0cf8c4e57c8003e2de4f
kgxkaran <karangera025@gmail.com>
2024-08-17 16:48:11 +0000

    let var and const in js

---
+++ 01_basics/variables.js
@@ -0,0 +1,11 @@
+// "Let" "Var" and "const"  in  JS
+
+let accountEmail = "kgera0010@gmail.com";  //We will use 'let' mostly
+var accountName ="karan";
+const accountId = 7584393;
+accountCity ="jaipur";
+
+console.table([accountEmail,accountName,accountId,accountCity]);
+
+//"console.table([])"  :-   Prints in tabular form
+//{} :-  Curly braces is called scopes
