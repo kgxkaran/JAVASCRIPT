191ec5cecd1a4bf939f68c78ec41d002470d5703
kgxkaran <karangera025@gmail.com>
2024-08-18 12:50:46 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version but aajkal itni advance aagyi hh ki khud smjh jaati h
+
+// Your code should always be readable
+
+// DATA TYPES IN JS :-
+
+// Number
+// STRING
+// Boolean
+// null  => Its basically an object
+// undefined   Eg:-  let variable; //will give output 'undefined'
+// symbol => unique
+// typeof operator => tells the type of variable
+
+console.log(typeof null); //object 
+
+console.log(typeof undefined);  //undefined
+
+
+
+
+
