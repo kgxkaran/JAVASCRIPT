8969d0e15ef359df2998c564875bdb1cc8c56004
kgxkaran <karangera025@gmail.com>
2024-08-27 18:06:15 +0000

    json talk

---
+++ 01_basics/apis.js
@@ -0,0 +1,15 @@
+//basics of APIs
+
+// {
+//     "name": "hitesh",                     //JSON ki keys bhi strings hoti and uski vslues bhi
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+//sometimes APIs are also found in array of objects
+
+[
+    {},
+    {},
+    {}
+]
